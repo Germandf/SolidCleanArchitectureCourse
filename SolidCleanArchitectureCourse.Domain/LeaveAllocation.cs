1f504acf2d20d5d464296c291c265117b886f650
German De Francesco <germandefrancesco@hotmail.com>
2023-01-30 19:59:37 -0300

    Add Domain project

---
+++ SolidCleanArchitectureCourse.Domain/LeaveAllocation.cs
@@ -0,0 +1,11 @@
+﻿using SolidCleanArchitectureCourse.Domain.Common;
+
+namespace SolidCleanArchitectureCourse.Domain;
+
+public class LeaveAllocation : BaseEntity
+{
+    public int NumberOfDays { get; set; }
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public int Period { get; set; }
+}
---
+++ SolidCleanArchitectureCourse.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿using SolidCleanArchitectureCourse.Domain.Common;
+
+namespace SolidCleanArchitectureCourse.Domain;
+
+public class LeaveType : BaseEntity
+{
+    public string Name { get; set; } = "";
+    public int DefaultDays { get; set; }
+}
