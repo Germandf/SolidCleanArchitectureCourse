732f25e4a4d2266ebaeba566dea52a2e3fe68be8
German De Francesco <germandefrancesco@hotmail.com>
2023-01-31 23:06:12 -0300

    Add Infrastructure Project with EmailService

---
+++ SolidCleanArchitectureCourse.Application/Contracts/Email/IEmailSender.cs
@@ -0,0 +1,8 @@
+﻿using SolidCleanArchitectureCourse.Application.Models.Email;
+
+namespace SolidCleanArchitectureCourse.Application.Contracts.Email;
+
+public interface IEmailSender
+{
+    Task<bool> SendEmail(EmailMessage email);
+}
---
+++ SolidCleanArchitectureCourse.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SolidCleanArchitectureCourse.Application.Contracts.Email;
+using SolidCleanArchitectureCourse.Application.Models.Email;
+using SolidCleanArchitectureCourse.Infrastructure.EmailService;
+
+namespace SolidCleanArchitectureCourse.Infrastructure;
+
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.Configure<EmailSettings>(configuration.GetSection(nameof(EmailSettings)));
+        services.AddTransient<IEmailSender, EmailSender>();
+
+        return services;
+    }
+}
