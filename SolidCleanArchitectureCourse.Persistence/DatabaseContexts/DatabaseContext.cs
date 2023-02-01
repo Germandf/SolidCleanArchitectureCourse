0dd1002c51dbf91d4322667b995024f8d6e03bb8
German De Francesco <germandefrancesco@hotmail.com>
2023-01-31 21:06:29 -0300

    Add Infrastructure's Persistence project with EF Core using SqlServer

---
+++ SolidCleanArchitectureCourse.Persistence/DatabaseContexts/DatabaseContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolidCleanArchitectureCourse.Domain;
+using SolidCleanArchitectureCourse.Domain.Common;
+
+namespace SolidCleanArchitectureCourse.Persistence.DatabaseContexts;
+
+public class DatabaseContext : DbContext
+{
+	public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+	{
+
+	}
+
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified))
+        {
+            entry.Entity.DateModified = DateTime.Now;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ SolidCleanArchitectureCourse.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SolidCleanArchitectureCourse.Persistence.DatabaseContexts;
+
+namespace SolidCleanArchitectureCourse.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.AddDbContext<DatabaseContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DatabaseContextConnectionString"));
+        });
+
+        return services;
+    }
+}
