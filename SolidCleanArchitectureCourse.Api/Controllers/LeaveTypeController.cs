cc8c598c8c6cd417754b9ca2ef34969e7e804e4e
German De Francesco <germandefrancesco@hotmail.com>
2023-02-01 22:39:13 -0300

    Add LeaveTypeController with CRUD endpoints

---
+++ SolidCleanArchitectureCourse.Api/Controllers/LeaveTypeController.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SolidCleanArchitectureCourse.Application.Features.LeaveType.Commands.CreateLeaveType;
+using SolidCleanArchitectureCourse.Application.Features.LeaveType.Commands.DeleteLeaveType;
+using SolidCleanArchitectureCourse.Application.Features.LeaveType.Commands.UpdateLeaveType;
+using SolidCleanArchitectureCourse.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using SolidCleanArchitectureCourse.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+
+namespace SolidCleanArchitectureCourse.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LeaveTypeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public LeaveTypeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<List<LeaveTypeDto>> Get()
+    {
+        return await _mediator.Send(new GetAllLeaveTypesQuery());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<LeaveTypeDetailsDto> Get(int id)
+    {
+        return await _mediator.Send(new GetLeaveTypeDetailsQuery(id));
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> Post([FromBody] CreateLeaveTypeCommand request)
+    {
+        return CreatedAtAction(nameof(Get), new { id = await _mediator.Send(request) });
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> Put([FromBody] UpdateLeaveTypeCommand request)
+    {
+        await _mediator.Send(request);
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> Delete(int id)
+    {
+        await _mediator.Send(new DeleteLeaveTypeCommand(id));
+        return NoContent();
+    }
+}
