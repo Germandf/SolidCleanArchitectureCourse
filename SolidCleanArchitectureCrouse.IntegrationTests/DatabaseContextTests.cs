86fbd19350fdead5072beda01a70cd298e1a5cff
German De Francesco <germandefrancesco@hotmail.com>
2023-02-05 02:18:53 -0300

    Add IntegrationTests project with DatabaseContextTests

---
+++ SolidCleanArchitectureCrouse.IntegrationTests/DatabaseContextTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using SolidCleanArchitectureCourse.Domain;
+using SolidCleanArchitectureCourse.Persistence.DatabaseContexts;
+
+namespace SolidCleanArchitectureCrouse.IntegrationTests;
+
+public class DatabaseContextTests
+{
+    private DatabaseContext _databaseContext;
+
+    public DatabaseContextTests()
+	{
+		var dbOptions = new DbContextOptionsBuilder<DatabaseContext>()
+			.UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+		_databaseContext = new DatabaseContext(dbOptions);
+	}
+
+	[Fact]
+	public async Task Save_SetDateCreatedValue()
+	{
+		var leaveType = new LeaveType
+		{
+			Id = 1,
+			DefaultDays = 10,
+			Name = "Test Vacation"
+		};
+
+		await _databaseContext.LeaveTypes.AddAsync(leaveType);
+		await _databaseContext.SaveChangesAsync();
+
+		leaveType.DateCreated.ShouldNotBeNull();
+	}
+
+    [Fact]
+    public async Task Save_SetDateModifiedValue()
+    {
+        var leaveType = new LeaveType
+        {
+            Id = 1,
+            DefaultDays = 10,
+            Name = "Test Vacation"
+        };
+
+        await _databaseContext.LeaveTypes.AddAsync(leaveType);
+        await _databaseContext.SaveChangesAsync();
+
+        leaveType.DateModified.ShouldNotBeNull();
+    }
+}
