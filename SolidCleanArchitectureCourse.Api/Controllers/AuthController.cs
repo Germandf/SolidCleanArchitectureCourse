1ac5ae91438ae3ee8bfcd68dc31b0fb3a63ae631
German De Francesco <germandefrancesco@hotmail.com>
2023-02-07 21:14:38 -0300

    Add Identity services and endpoints in Api

---
+++ SolidCleanArchitectureCourse.Api/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SolidCleanArchitectureCourse.Application.Identity;
+using SolidCleanArchitectureCourse.Application.Models.Identity;
+
+namespace SolidCleanArchitectureCourse.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("Login")]
+    public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+    {
+        return Ok(await _authService.Login(request));
+    }
+
+    [HttpPost("Register")]
+    public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+    {
+        return Ok(await _authService.Register(request));
+    }
+}
--- SolidCleanArchitectureCourse.Api/Program.cs
+++ SolidCleanArchitectureCourse.Api/Program.cs
@@ -1,12 +1,16 @@
 using SolidCleanArchitectureCourse.Api.Middlewares;
 using SolidCleanArchitectureCourse.Application;
+using SolidCleanArchitectureCourse.Identity;
 using SolidCleanArchitectureCourse.Infrastructure;
 using SolidCleanArchitectureCourse.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
 builder.Services.AddControllers();
 builder.Services.AddCors(options => options.AddPolicy("all", builder => 
     builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
@@ -14,11 +18,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseCors("all");
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
